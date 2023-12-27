2c078824342936d364f528d6ce7375212d759c89
Raims Cabrera <raimscabrera@gmail.com>
2023-12-27 11:02:32 +0800

    Add support for different heading levels in PageHeading component

--- src/components/PageHeading.tsx
+++ src/components/PageHeading.tsx
@@ -6,6 +6,46 @@ type PageHeadingProps = {
 }
 
 const PageHeading = ({ children, level = 1 }: PageHeadingProps) => {
+  if (level === 2) {
+    return (
+      <h2 className='text-4xl font-bold text-center'>
+        {children}
+      </h2>
+    )
+  }
+
+  if (level === 3) {
+    return (
+      <h3 className='text-3xl font-bold text-center'>
+        {children}
+      </h3>
+    )
+  }
+
+  if (level === 4) {
+    return (
+      <h4 className='text-2xl font-bold text-center'>
+        {children}
+      </h4>
+    )
+  }
+
+  if (level === 5) {
+    return (
+      <h5 className='text-xl font-bold text-center'>
+        {children}
+      </h5>
+    )
+  }
+
+  if (level === 6) {
+    return (
+      <h6 className='text-lg font-bold text-center'>
+        {children}
+      </h6>
+    )
+  }
+
   return (
     <h1 className='text-5xl font-bold text-center'>
       {children}
