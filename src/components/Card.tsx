6e55858c652ce93c07bf5cdd525f525eef22887b
Raims Cabrera <raimscabrera@gmail.com>
2024-04-11 18:20:44 +0800

    More changes

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -29,19 +29,19 @@ const Card = ({ date, slug, title, type, image, author }: CardProps) => {
   }
 
   return (
-    <Link to={`/${type}${slug}`}>
-      <article className='px-4 py-8 w-96 hover:bg-white hover:shadow-lg transition-all duration ease-out rounded'>
+    <article className='px-4 py-8 w-96 hover:bg-white hover:shadow-lg transition-all duration ease-out rounded margin-auto justify-self-center'>
+      <Link to={`/${type}${slug}`}>
         <p className='uppercase text-sm text-gray-400 tracking-widest font-semibold leading-none'>
           {getDate()}
         </p>
         <PageHeading level={2} classes='mb-2'>{title}</PageHeading>
-        <p>{author}</p>
+        <p className='mb-1'>{author}</p>
         <img src={image.publicURL} alt={title} className='h-96 w-auto object-cover mb-4' />
         <section className='flex items-center justify-start gap-4'>
           <Tag type={type} />
         </section>
-      </article>
-    </Link>
+      </Link>
+    </article>
   )
 }
 
